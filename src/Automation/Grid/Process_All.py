33d254d62ce1f2a327b516a8470471594311bdad
Hans Bakker <jjabakker@icloud.com>
2024-10-04 10:03:40 +0200

    Logging

--- src/Automation/Grid/Process_All.py
+++ src/Automation/Grid/Process_All.py
@@ -1,11 +1,14 @@
 import os
 import shutil
-import logging
+import time
 
 from src.Automation.Grid.Generate_Squares_Single import process_images_in_root_directory_single_mode
+
 from src.Automation.Grid.Generate_Squares_Traditional import process_images_in_root_directory_traditional_mode
+
 from src.Automation.Grid.Compile_Results_Files import compile_squares_file
-from src.Automation.Grid.Copy_Data_From_Source_ChatGPT import copy_data_from_source
+from src.Automation.Support.Copy_Data_From_Source import copy_data_from_source
+from src.Automation.Support.Directory_Timestamp import set_directory_timestamp
 from src.Automation.Support.Logger_Config import logger
 
 SOURCE_NEW_DIR     = '/Users/hans/Paint Source/New Probes'
@@ -16,16 +19,15 @@ paint_source_dirs = {
     'regular':  SOURCE_REGULAR_DIR
 }
 
+logger.debug("\n\n\n\nNew Run\n\n\n")
 
-
-logger.info("\n\n\n\nNew Run\n\n\n")
 
 def copy_directory(src, dest):
     try:
         if os.path.exists(dest):
             shutil.rmtree(dest)
         shutil.copytree(src, dest)
-        logging.info(f"Copied {src} to {dest}")
+        logger.debug(f"Copied {src} to {dest}")
     except Exception as e:
         logger.error(f"process_all_chatgtp - copy directories: Failed to copy {src} to {dest}. Error: {e}")
 
@@ -62,33 +64,39 @@ def run_traditional(root_dir, nr_of_squares, min_density_ratio):
 
 
 def process_directory(directory, root_dir, dest_dir, mode, probe, nr_of_squares, min_density_ratio=None):
-    logger.info(f"\nProcessing directory {directory}")
 
+    time_stamp = time.time()
+    logger.info("")
+    logger.info(f"Processing mode: {mode} - probe {probe} - directory: {directory}")
     copy_data_from_source(paint_source_dirs[probe], root_dir)
 
     if not os.path.exists(dest_dir):
         os.makedirs(dest_dir)
 
     if mode == 'single':
+
         run_single(root_dir, nr_of_squares)
     elif mode == 'traditional':
         run_traditional(root_dir, nr_of_squares, min_density_ratio)
 
-    compile_squares_file(root_dir, verbose=False)
+    compile_squares_file(root_dir, verbose=True)
     copy_directory(os.path.join(root_dir, 'Output'), os.path.join(dest_dir, 'Output'))
+    set_directory_timestamp(root_dir)
+    set_directory_timestamp(dest_dir)
+    logger.info(f"Processed mode: {mode} - probe {probe} - directory: {directory} in {time.time() - time_stamp:.1f} seconds")
 
 
 # Define the configuration for different processing modes
 config = [
     # Single modes
-    {'flag': False,
+    {'flag': True,
      'mode': 'single',
      'probe': 'new',
      'directory': 'Paint New Probes - Single - 30 Squares - 10 DR',
      'source_dir': '/Users/hans/Paint Data/New Probes/Single/',
      'nr_of_squares': 30},
 
-    {'flag': False,
+    {'flag': True,
      'mode': 'single',
      'probe': 'new',
      'directory': 'Paint New Probes - Single - 30 Squares - 30 DR',
@@ -146,6 +154,13 @@ config = [
 root_dest_dir = '/Users/hans/Documents/LST/Master Results/PAINT Pipeline/Code/Paint-R/Data/'
 
 # Main loop to process each configuration based on flags
+
+main_stamp = time.time()
+
+logger.info("")
+logger.info('Starting the full processing')
+logger.info("")
+
 for entry in config:
     if entry['flag']:
         root_dir = os.path.join(entry['source_dir'], entry['directory'])
@@ -159,3 +174,12 @@ for entry in config:
             nr_of_squares=entry['nr_of_squares'],
             min_density_ratio=entry.get('min_density_ratio')
         )
+
+# Report the time it took in hours minutes seconds
+run_time = time.time() - main_stamp
+hours, remainder = divmod(run_time, 3600)
+minutes, seconds = divmod(run_time, 60)
+
+logger.info("")
+logger.info(f'Finished the full processing in  {hours} hours {minutes} minutes and {seconds} seconds')
+logger.info("")
