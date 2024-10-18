946ff909c0af3098aa6122e1839df788eaf7817a
Hans Bakker <jjabakker@icloud.com>
2024-10-18 15:12:50 +0200

    Added BF Convert functionality

--- src/Application/Convert BF images Python.py
+++ src/Application/Convert BF images Python.py
@@ -1,116 +1,68 @@
-import os
-import shutil
-
-import numpy as np
-from PIL import Image
-from nd2reader import ND2Reader
-
-from src.Common.Support.LoggerConfig import paint_logger
 
 
-def convert_nd2_to_jpg(nd2_file_path, output_file):
-    # Open the .nd2 file
-    with ND2Reader(nd2_file_path) as images:
-
-        frame = images[0]
-        # Check if the frame is in 16-bit format
-        if frame.dtype == np.uint16:
-            # Normalize the 16-bit frame to the range 0-255 (8-bit)
-            min_val = np.min(frame)
-            max_val = np.max(frame)
-
-            # Avoid division by zero in case the image is uniform
-            if max_val > min_val:
-                frame = ((frame - min_val) / (max_val - min_val) * 255).astype(np.uint8)
-            else:
-                frame = np.zeros_like(frame, dtype=np.uint8)  # Handle uniform image case
-
-        # Convert frame to PIL Image
-        img = Image.fromarray(frame)
-
-        # Save the image as .jpg
-        img.save(output_file, 'JPEG')
-
-
-def convert_bf_images(image_source_directory, paint_directory, force=False):
-    """
-    Convert .nd2 BF images to JPEG and store them in a specified directory.
-
-    Args:
-        image_source_directory (str): Directory containing the .nd2 images.
-        paint_directory (str): Directory to store the converted JPEGs.
-        force (bool): Force overwrite of existing JPEG files, even if up to date.
-    """
-    # Create a 'Converted BF Images' directory if it doesn't exist
-    bf_jpeg_dir = os.path.join(image_source_directory, "Converted BF Images")
-    if not os.path.isdir(bf_jpeg_dir):
-        os.mkdir(bf_jpeg_dir)
-
-    paint_logger.info('')  # Start logging new run
-
-    count = found = converted = 0
-    all_images = sorted(os.listdir(image_source_directory))  # Sort images for predictable processing order
-
-    for image_name in all_images:
-        # Skip hidden files or system files
-        if image_name.startswith('._'):
-            continue
-
-        # Check if the file is a .nd2 file
-        if image_name.endswith('.nd2'):
-            count += 1
-
-            # Only process Bright Field (BF) images
-            if 'BF' in image_name:
-                found += 1
-                display_name = image_name.ljust(30, ' ')  # Align name in log for readability
-                input_file = os.path.join(image_source_directory, image_name)
-                output_file = os.path.join(bf_jpeg_dir, image_name.replace('.nd2', '.jpg'))
-
-                # Determine if the image needs to be converted (force flag or file modification check)
-                convert = force or not os.path.isfile(output_file) or os.path.getmtime(output_file) < os.path.getmtime(
-                    input_file)
-
-                if convert:
-                    try:
-                        convert_nd2_to_jpg(input_file, output_file)
-                        paint_logger.info("Image %s was updated.", display_name)
-                        converted += 1
-                    except Exception as e:
-                        paint_logger.error("Error converting %s: %s", display_name, str(e))
-                else:
-                    paint_logger.info("Image %s does not require updating.", display_name)
-
-    # Log the conversion summary
-    paint_logger.info("\nConverted %d BF images, out of %d BF images from %d total .nd2 images.", converted, found,
-                      count)
-
-    # Copy the entire 'Converted BF Images' directory to the paint directory
-    dest_dir = os.path.join(paint_directory, "Converted BF Images")
-    if os.path.exists(dest_dir):
-        # If the destination directory already exists, remove it before copying
-        shutil.rmtree(dest_dir)
-
-    try:
-        shutil.copytree(bf_jpeg_dir, dest_dir)
-        paint_logger.info("Copied the entire 'Converted BF Images' directory to %s", dest_dir)
-    except Exception as e:
-        paint_logger.error("Error copying the directory %s to %s: %s", bf_jpeg_dir, dest_dir, str(e))
-
+from tkinter import filedialog, messagebox, ttk
+from src.Application.Support.Convert_BF_from_nd2_to_jpg import convert_bf_images
 
 if __name__ == "__main__":
-    # Ask user for Paint directory
-    # paint_directory = ask_user_for_image_directory("Specify the Paint directory", 'Paint')
-    # if not paint_directory:
-    #     paint_logger.info("User aborted the batch processing.")
-    #     sys.exit(0)
-    #
-    # # Ask user for the source directory of images
-    # images_source_directory = ask_user_for_image_directory("Specify the Image Source directory", 'Images')
-    # if not images_source_directory:
-    #     paint_logger.info("User aborted the batch processing.")
-    #     sys.exit(0)
-    #
-    # # Run the conversion with user-specified directories
-    # convert_bf_images(images_source_directory, paint_directory, force=False)
-    convert_bf_images('/Volumes/Extreme Pro/Omero/221101', '/Users/hans/Downloads/Test', force=True)
+    class BatchDialog:
+
+        def __init__(self, root):
+            self.root = root
+            self.root.title('Convert BF Images from .nd2 to .jpg')
+
+            self.image_directory = ""
+            self.paint_directory = ""
+
+            content = ttk.Frame(self.root)
+            frame_buttons = ttk.Frame(content, borderwidth=5, relief='ridge')
+            frame_directory = ttk.Frame(content, borderwidth=5, relief='ridge')
+
+            #  Do the lay-out
+            content.grid(column=0, row=0)
+            frame_directory.grid(column=0, row=1, padx=5, pady=5)
+            frame_buttons.grid(column=0, row=2, padx=5, pady=5)
+
+            # Fill the button frame
+            btn_process = ttk.Button(frame_buttons, text='Process', command=self.process)
+            btn_exit = ttk.Button(frame_buttons, text='Exit', command=self.exit_dialog)
+            btn_process.grid(column=0, row=1)
+            btn_exit.grid(column=0, row=2)
+
+            # Fill the directory frame
+            btn_image_dir = ttk.Button(frame_directory, text='Image Source Directory', width=20,
+                                       command=self.change_image_dir)
+            self.lbl_image_dir = ttk.Label(frame_directory, text=self.image_directory, width=50)
+
+            btn_paint_dir = ttk.Button(frame_directory, text='Experiment Directory', width=20,
+                                       command=self.change_paint_dir)
+            self.lbl_paint_dir = ttk.Label(frame_directory, text=self.paint_directory, width=50)
+
+            btn_image_dir.grid(column=0, row=0, padx=10, pady=5)
+            self.lbl_image_dir.grid(column=1, row=0, padx=20, pady=5)
+
+            btn_paint_dir.grid(column=0, row=1, padx=10, pady=5)
+            self.lbl_paint_dir.grid(column=1, row=1, padx=20, pady=5)
+
+        def change_image_dir(self):
+            self.image_directory = filedialog.askdirectory(initialdir=self.image_directory)
+            if len(self.image_directory) != 0:
+                self.lbl_image_dir.config(text=self.image_directory)
+
+        def change_paint_dir(self):
+            self.paint_directory = filedialog.askdirectory(initialdir=self.paint_directory)
+            if len(self.paint_directory) != 0:
+                self.lbl_paint_dir.config(text=self.paint_directory)
+
+        def process(self):
+            if self.image_directory == "" or self.paint_directory == "":
+                message = 'The image directory needs to point to where the images are.\n\n'
+                message += 'The experiment directory is where the experiment_info.csv will be placed.'
+                messagebox.showwarning(title='Warning', message=message)
+            else:
+                # prepare_experiment_info_file(self.image_directory, self.paint_directory)
+                self.exit_dialog()
+
+        def exit_dialog(self):
+            self.root.destroy()
+
+
--- src/Application/Prepare for Analysis.py
+++ src/Application/Prepare for Analysis.py
@@ -10,6 +10,7 @@ from src.Common.Support.LoggerConfig import (
     paint_logger,
     paint_logger_change_file_handler_name)
 
+from src.Application.Support.Convert_BF_from_nd2_to_jpg import convert_bf_images
 
 def prepare_experiment_info_file(image_source_directory, experiment_directory):
     """
@@ -78,9 +79,6 @@ def prepare_experiment_info_file(image_source_directory, experiment_directory):
 
         paint_logger.info(f'Processing file: {image_name}')
 
-        # Get the image size
-        image_size = os.path.getsize(os.path.join(image_source_directory, image_name))
-
         image_name = image_name.replace(".nd2", "")
         row = {'Batch Sequence Nr': seq_nr,
                'Experiment Date': exp_date,
@@ -120,70 +118,73 @@ def prepare_experiment_info_file(image_source_directory, experiment_directory):
     paint_logger.info("Then choose the threshold values and select which images needs processing.")
 
 
-class BatchDialog:
+if __name__ == '__main__':
 
-    def __init__(self, _root):
-        _root.title('Prepare batch file')
+    class BatchDialog:
 
-        self.image_directory = ""
-        self.paint_directory = ""
+        def __init__(self, _root):
+            _root.title('Prepare batch file')
 
-        content = ttk.Frame(_root)
-        frame_buttons = ttk.Frame(content, borderwidth=5, relief='ridge')
-        frame_directory = ttk.Frame(content, borderwidth=5, relief='ridge')
+            self.image_directory = ""
+            self.paint_directory = ""
 
-        #  Do the lay-out
-        content.grid(column=0, row=0)
-        frame_directory.grid(column=0, row=1, padx=5, pady=5)
-        frame_buttons.grid(column=0, row=2, padx=5, pady=5)
+            content = ttk.Frame(_root)
+            frame_buttons = ttk.Frame(content, borderwidth=5, relief='ridge')
+            frame_directory = ttk.Frame(content, borderwidth=5, relief='ridge')
 
-        # Fill the button frame
-        btn_process = ttk.Button(frame_buttons, text='Process', command=self.process)
-        btn_exit = ttk.Button(frame_buttons, text='Exit', command=self.exit_dialog)
-        btn_process.grid(column=0, row=1)
-        btn_exit.grid(column=0, row=2)
+            #  Do the lay-out
+            content.grid(column=0, row=0)
+            frame_directory.grid(column=0, row=1, padx=5, pady=5)
+            frame_buttons.grid(column=0, row=2, padx=5, pady=5)
 
-        # Fill the directory frame
-        btn_image_dir = ttk.Button(frame_directory, text='Image Source Directory', width=20,
-                                   command=self.change_image_dir)
-        self.lbl_image_dir = ttk.Label(frame_directory, text=self.image_directory, width=50)
+            # Fill the button frame
+            btn_process = ttk.Button(frame_buttons, text='Process', command=self.process)
+            btn_exit = ttk.Button(frame_buttons, text='Exit', command=self.exit_dialog)
+            btn_process.grid(column=0, row=1)
+            btn_exit.grid(column=0, row=2)
 
-        btn_paint_dir = ttk.Button(frame_directory, text='Experiment Directory', width=20,
-                                   command=self.change_paint_dir)
-        self.lbl_paint_dir = ttk.Label(frame_directory, text=self.paint_directory, width=50)
+            # Fill the directory frame
+            btn_image_dir = ttk.Button(frame_directory, text='Image Source Directory', width=20,
+                                       command=self.change_image_dir)
+            self.lbl_image_dir = ttk.Label(frame_directory, text=self.image_directory, width=50)
 
-        btn_image_dir.grid(column=0, row=0, padx=10, pady=5)
-        self.lbl_image_dir.grid(column=1, row=0, padx=20, pady=5)
+            btn_paint_dir = ttk.Button(frame_directory, text='Experiment Directory', width=20,
+                                       command=self.change_paint_dir)
+            self.lbl_paint_dir = ttk.Label(frame_directory, text=self.paint_directory, width=50)
 
-        btn_paint_dir.grid(column=0, row=1, padx=10, pady=5)
-        self.lbl_paint_dir.grid(column=1, row=1, padx=20, pady=5)
+            btn_image_dir.grid(column=0, row=0, padx=10, pady=5)
+            self.lbl_image_dir.grid(column=1, row=0, padx=20, pady=5)
 
-    def change_image_dir(self):
-        self.image_directory = filedialog.askdirectory(initialdir=self.image_directory)
-        if len(self.image_directory) != 0:
-            self.lbl_image_dir.config(text=self.image_directory)
+            btn_paint_dir.grid(column=0, row=1, padx=10, pady=5)
+            self.lbl_paint_dir.grid(column=1, row=1, padx=20, pady=5)
 
-    def change_paint_dir(self):
-        self.paint_directory = filedialog.askdirectory(initialdir=self.paint_directory)
-        if len(self.paint_directory) != 0:
-            self.lbl_paint_dir.config(text=self.paint_directory)
+        def change_image_dir(self):
+            self.image_directory = filedialog.askdirectory(initialdir=self.image_directory)
+            if len(self.image_directory) != 0:
+                self.lbl_image_dir.config(text=self.image_directory)
 
-    def process(self):
-        if self.image_directory == "" or self.paint_directory == "":
-            message = 'The image directory needs to point to where the images are.\n\n'
-            message += 'The experimen directory is where the experiment_info.csv will be placed.'
-            messagebox.showwarning(title='Warning', message=message)
-        else:
-            prepare_experiment_info_file(self.image_directory, self.paint_directory)
-            self.exit_dialog()
+        def change_paint_dir(self):
+            self.paint_directory = filedialog.askdirectory(initialdir=self.paint_directory)
+            if len(self.paint_directory) != 0:
+                self.lbl_paint_dir.config(text=self.paint_directory)
 
-    def exit_dialog(self):
-        root.destroy()
+        def process(self):
+            if self.image_directory == "" or self.paint_directory == "":
+                message = 'The image directory needs to point to where the images are.\n\n'
+                message += 'The experimen directory is where the experiment_info.csv will be placed.'
+                messagebox.showwarning(title='Warning', message=message)
+            else:
+                convert_bf_images(self.image_directory, self.paint_directory)
+                prepare_experiment_info_file(self.image_directory, self.paint_directory)
+                self.exit_dialog()
+
+        def exit_dialog(self):
+            root.destroy()
 
 
-paint_logger_change_file_handler_name('Prepare Experiment_Info File.log')
+    paint_logger_change_file_handler_name('Prepare Experiment_Info File.log')
 
-root = Tk()
-root.eval('tk::PlaceWindow . center')
-BatchDialog(root)
-root.mainloop()
+    root = Tk()
+    root.eval('tk::PlaceWindow . center')
+    BatchDialog(root)
+    root.mainloop()
