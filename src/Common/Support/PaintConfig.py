c7595a1c4d2e3575506e7b3ded28ab7232c39295
Hans Bakker <jjabakker@icloud.com>
2024-10-28 14:52:50 +0100

    Lazy init

--- src/Common/Support/PaintConfig.py
+++ src/Common/Support/PaintConfig.py
@@ -1,23 +1,23 @@
 import os
 import json
 
-class PaintConfiguration:
-    _instance = None
-    paint_conf_data = None
 
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(PaintConfiguration, cls).__new__(cls)
-            cls.paint_conf_data = load_paint_config(os.path.join(os.path.expanduser('~'), 'Paint', 'Defaults', 'paint.json'))
-        return cls._instance
-
+paint_configuration = None
 
 # Load configuration from a JSON file
 def load_paint_config(file_path):
+
+    global paint_configuration
+
+    if paint_configuration is not None:
+        return paint_configuration
+
+    if file_path is None:
+        file_path = os.path.join(os.path.expanduser('~'), 'Paint', 'Defaults', 'paint.json')
     try:
         with open(file_path, 'r') as config_file:
-            config = json.load(config_file)
-        return config
+            paint_configuration = json.load(config_file)
+        return paint_configuration
     except FileNotFoundError:
         print("Error: Configuration file {} not found.".format(file_path))
         #paint_logger.error("Error: Configuration file {} not found.".format(file_path))
@@ -28,28 +28,16 @@ def load_paint_config(file_path):
         return None
 
 
+
 if __name__ == '__main__':
 
 
-    paint_config1 = PaintConfiguration().paint_conf_data
-    paint_config2 = PaintConfiguration().paint_conf_data
-    paint_config3 = PaintConfiguration().paint_conf_data
+    config = load_paint_config(os.path.join(os.path.expanduser('~'), 'Paint', 'Defaults', 'paint.json'))
+    trackmate_config = config['TrackMate']
+    max_gap1 = trackmate_config['MAX_FRAME_GAP']
 
-    print(paint_config3)
+    config = load_paint_config(os.path.join(os.path.expanduser('~'), 'Paint', 'Defaults', 'paint.json'))
+    trackmate_config = config['TrackMate']
+    max_gap1 = trackmate_config['MAX_FRAME_GAP']
 
-    if paint_config2:
-
-        trackmate_defaults1 = paint_config1['TrackMate']
-        trackmate_defaults2 = paint_config2['TrackMate']
-        trackmate_defaults3 = paint_config3['TrackMate']
-
-        max_gap1 = trackmate_defaults1['MAX_FRAME_GAP']
-        max_gap2 = trackmate_defaults1['MAX_FRAME_GAP']
-
-
-        config = load_paint_config(os.path.join(os.path.expanduser('~'), 'Paint', 'Defaults', 'paint.json'))
-        plot = config['Generate Squares']['Plot to File']
-
-        max_gap1 = config['TrackMate']['MAX_FRAME_GAP']
-
-        i = 1
+    i = 1
