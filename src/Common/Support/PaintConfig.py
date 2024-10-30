eaa26ac982443a1b6e30bb73885b9c5daaca59d0
Hans Bakker <jjabakker@icloud.com>
2024-10-30 14:52:24 +0100

    Make Jython proof
    get_paint_defaults_file_path can not be used becaus eit references the src directory, so define a local version

--- src/Common/Support/PaintConfig.py
+++ src/Common/Support/PaintConfig.py
@@ -1,7 +1,8 @@
 import os
 import json
 
-from src.Common.Support.DirectoriesAndLocations import get_paint_defaults_file_path
+def get_paint_defaults_file_path():     #ToDo
+    return os.path.join(os.path.expanduser('~'), 'Paint', 'Config', 'Paint.json')
 
 paint_configuration = None
 
@@ -19,15 +20,18 @@ def load_paint_config(file_path):
         with open(file_path, 'r') as config_file:
             paint_configuration = json.load(config_file)
         return paint_configuration
-    except FileNotFoundError:
-        print("Error: Configuration file {} not found.".format(file_path))
-        #paint_logger.error("Error: Configuration file {} not found.".format(file_path))
+    # except FileNotFoundError:
+    #     print("Error: Configuration file {} not found.".format(file_path))
+    #     paint_logger.error("Error: Configuration file {} not found.".format(file_path))
+    #     return None
+    # except json.JSONDecodeError:
+    #     paint_logger.error("Failed to parse JSON from {}.".format(file_path))
+    #     print("Failed to parse JSON from {}.".format(file_path))
+    #     return None
+    except:
+        print("Error: Problem with configuration file {}.".format(file_path))
+        paint_logger.error("Error: Configuration file {} not found.".format(file_path))
         return None
-    except json.JSONDecodeError:
-        #paint_logger.error("Failed to parse JSON from {}.".format(file_path))
-        print("Failed to parse JSON from {}.".format(file_path))
-        return None
-
 
 def get_paint_attribute(application,  attribute_name):
     config = load_paint_config(get_paint_defaults_file_path())
@@ -47,6 +51,6 @@ if __name__ == '__main__':
 
     config = load_paint_config(os.path.join(os.path.expanduser('~'), 'Paint', 'Defaults', 'paint.json'))
     trackmate_config = config['TrackMate']
-    max_gap1 = trackmate_config['MAX_FRAME_GAP']
+    max_gap = trackmate_config['MAX_FRAME_GAP']
 
     i = 1
