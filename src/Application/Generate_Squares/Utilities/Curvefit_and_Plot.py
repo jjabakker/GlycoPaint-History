2db720233128f738fa1b5bc180f4ed36deda8021
Hans Bakker <jjabakker@icloud.com>
2024-11-11 13:37:57 +0100

    Comments

--- src/Application/Generate_Squares/Utilities/Curvefit_and_Plot.py
+++ src/Application/Generate_Squares/Utilities/Curvefit_and_Plot.py
@@ -10,48 +10,6 @@ from scipy.optimize import curve_fit
 from src.Common.Support.LoggerConfig import paint_logger
 
 
-def read_track_mate_data(csvfilename, istrack):
-    """
-    Function is not to be called externally, but by ReadTracksData or ReadSpotsData
-    Read in the data file (it can be either 'tracks' or 'spots').
-    Row 0 contains the header.
-    Rows 1, 2 and 3 contain commentary, so skip those.
-    :param csvfilename:
-    :param istrack: A boolean value indicating whether it is tracks data (True) or spots data (False)
-    :return: the dataframe with tracks
-    """
-
-    try:
-        tmd = pd.read_csv(csvfilename, header=0, skiprows=[1, 2, 3])
-    except FileNotFoundError:
-        paint_logger.error(f'Could not open {csvfilename}')
-        sys.exit()
-    except (pd.errors.ParserError, UnicodeDecodeError):
-        paint_logger.error(f'Problem parsing {csvfilename}')
-        sys.exit()
-
-    # Drop unused columns for 'tracks' data or 'plots' data
-    try:
-        if istrack:
-            tmd.drop(['NUMBER_SPLITS', 'NUMBER_MERGES', 'TRACK_Z_LOCATION',
-                      'NUMBER_COMPLEX'], axis=1, inplace=True)
-        else:
-            tmd.drop(['POSITION_Z', 'MANUAL_SPOT_COLOR'], axis=1, inplace=True)
-        return tmd
-    except KeyError:
-        paint_logger.error(f'Unexpected column names in {csvfilename}')
-        sys.exit()
-
-
-def read_tracks_data(csvfilename):
-    """
-    :param csvfilename:
-    :return:
-    """
-
-    return read_track_mate_data(csvfilename, istrack=True)
-
-
 def mono_exp(x, m, t, b):
     # Define the exponential decay function that will be used for fitting
     try:
@@ -70,17 +28,24 @@ def mono_exp(x, m, t, b):
 
 def compile_duration(tracks):
     """
-    The function produces a histogram
+    The function produces a frequency distribution of the track durations.
     :param tracks: a dataframe containing the histogram data
-    :return: a dataframe containing the histogram
+    :return: a dataframe with two columns containing the histogram
     """
-
-    duration_data = tracks.groupby('Track Duration')['Track Duration'].size()
 
     # histdata is returned as a Pandas Series, make histdata into a dataframe
     # The index values are the duration and the first (and only) column is 'Frequency'
-    histdata = pd.DataFrame(duration_data)
+    histdata = tracks.groupby('Track Duration')['Track Duration'].size()
+    histdata = pd.DataFrame(histdata)
     histdata.columns = ['Frequency']
+    histdata['Track Duration'] = histdata.index
+
+    # This is alternative code to the above
+    # histdata_1 = tracks['Track Duration'].value_counts().reset_index()
+    # histdata_1.columns = ['Track Duration', 'Frequency']
+    # histdata_1 = histdata_1.sort_values(by='Track Duration')
+    # histdata_1 = histdata_1.set_index('Track Duration', drop=False)
+
     return histdata
 
 
@@ -95,20 +60,15 @@ def curve_fit_and_plot(plot_data, nr_tracks, plot_max_x, plot_title='Duration Hi
     :param plot_to_screen:
     :param plot_to_file
     :param verbose:
-    :return: nothing
+    :return: The Tau value in milliseconds and the R2
     """
 
-    # Convert the pd dataframe to Numpy arrays fur further curve fitting
-
-    x = list(plot_data.index)
-    x = np.asarray(x)
-
-    y = list(plot_data["Frequency"])
-    y = np.asarray(y)
+    # The curve_fit functiion expects x and y Numpy arrays
+    x = np.asarray(plot_data["Track Duration"])
+    y = np.asarray(plot_data["Frequency"])
 
     # Perform the fit
     p0 = [2000, 4, 10]  # this is more what we see
-
     try:
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", category=RuntimeWarning)
@@ -133,9 +93,9 @@ def curve_fit_and_plot(plot_data, nr_tracks, plot_max_x, plot_title='Duration Hi
             paint_logger.warning('CurveFitAndPlot: Exception')
         return -2, 0
 
-    tau_sec = (1 / t)
+    tau_per_sec = (1 / t)
 
-    # Determine quality of the fit
+    # Determine the quality of the fit
     squared_diffs = np.square(y - mono_exp(x, m, t, b))
     squared_diffs_from_mean = np.square(y - np.mean(y))
     if np.sum(squared_diffs_from_mean) == 0:
@@ -154,7 +114,7 @@ def curve_fit_and_plot(plot_data, nr_tracks, plot_max_x, plot_title='Duration Hi
 
         x_middle = plot_max_x / 2 - plot_max_x * 0.1
         y_middle = y.max() / 2
-        plt.text(x_middle, y_middle, f"Tau = {tau_sec * 1e3:.0f} ms")
+        plt.text(x_middle, y_middle, f"Tau = {tau_per_sec * 1e3:.0f} ms")
         plt.text(x_middle, 0.8 * y_middle, f"R2 = {r_squared:.4f} ms")
         plt.text(x_middle, 0.6 * y_middle, f"Number or tracks is {nr_tracks}")
         plt.text(x_middle, 0.4 * y_middle, f"Zoomed in from 0 to {plot_max_x:.0f} s")
@@ -181,8 +141,10 @@ def curve_fit_and_plot(plot_data, nr_tracks, plot_max_x, plot_title='Duration Hi
         print("")
         print(f'R² = {r_squared:.4f}')
         print(f'Y = {m:.3f} * e^(-{t:.3f} * x) + {b:.3f}')
-        print(f'Tau = {tau_sec * 1e3:.0f} ms')
+        print(f'Tau = {tau_per_sec * 1e3:.0f} ms')
 
     plt.close()
-    tau_sec *= 1000
-    return tau_sec, r_squared
+
+    # Convert to milliseconds
+    tau_per_sec *= 1000
+    return tau_per_sec, r_squared
