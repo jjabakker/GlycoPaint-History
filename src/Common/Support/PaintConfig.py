8bd33bc41679399f88e06c4e03aca60407a344d1
Hans Bakker <jjabakker@icloud.com>
2024-11-13 11:13:43 +0100

    Add paint_logger support in PaintConfig so that it also can be executed in Jython

--- src/Common/Support/PaintConfig.py
+++ src/Common/Support/PaintConfig.py
@@ -1,12 +1,17 @@
 import json
 import os
 
-#from src.Common.Support.LoggerConfig import paint_logger
-
 
 def get_paint_defaults_file_path():  # ToDo
     return os.path.join(os.path.expanduser('~'), 'Paint', 'Defaults', 'Paint.json')
 
+# This is unusual code. The import is only sup[osed to be done from the Jython environmen.
+# There LoggerConfig will be found. If it is seen in Python it will generate an error, but that does not matter
+
+try:
+    from LoggerConfig import paint_logger
+except:
+    pass
 
 paint_configuration = None
 
@@ -24,31 +29,31 @@ def load_paint_config(file_path):
         with open(file_path, 'r') as config_file:
             paint_configuration = json.load(config_file)
         return paint_configuration
-    # except FileNotFoundError:
-    #     print("Error: Configuration file {} not found.".format(file_path))
-    #     paint_logger.error("Error: Configuration file {} not found.".format(file_path))
-    #     return None
-    # except json.JSONDecodeError:
-    #     paint_logger.error("Failed to parse JSON from {}.".format(file_path))
-    #     print("Failed to parse JSON from {}.".format(file_path))
-    #     return None
+    except IOError:
+        print("Error: Configuration file {} not found.".format(file_path))
+        paint_logger.error("Error: Configuration file {} not found.".format(file_path))
+        return None
+    except ValueError:
+        paint_logger.error("Failed to parse JSON from {}.".format(file_path))
+        print("Failed to parse JSON from {}.".format(file_path))
+        return None
     except:
         print("Error: Problem with configuration file {}.".format(file_path))
-        # paint_logger.error("Error: Configuration file {} not found.".format(file_path))
+        paint_logger.error("Error: Configuration file {} not found.".format(file_path))
         return None
 
 
 def get_paint_attribute(application, attribute_name):
     config = load_paint_config(get_paint_defaults_file_path())
     if config is None:
-        #paint_logger.error("Error: Configuration file {} not found.".format(get_paint_defaults_file_path()))
+        paint_logger.error("Error: Configuration file {} not found.".format(get_paint_defaults_file_path()))
         return None
     else:
         application = config.get(application)
         value = application.get(attribute_name, None)
         if value is None:
             pass    #ToDo
-            # paint_logger.error("Error: Attribute {} not found in configuration file {}.".format(attribute_name, get_paint_defaults_file_path()))
+            paint_logger.error("Error: Attribute {} not found in configuration file {}.".format(attribute_name, get_paint_defaults_file_path()))
         return value
 
 
